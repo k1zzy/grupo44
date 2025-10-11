cae0902b9c48017e99073f2d4dcc046e0b9b9879
Rodrigo Afonso <fc61839@alunos.fc.ul.pt>
2025-10-11 16:31:59 +0100

    initial push

---
+++ include/message-private.h
@@ -0,0 +1,13 @@
+/**
+ * Projeto: Sistemas Distribuídos 2025/2026
+ * Autor: José Cecílio
+ * Data: 4/10/2025
+ */
+#ifndef _MESSAGE_PRIVATE_H
+#define _MESSAGE_PRIVATE_H
+
+int write_all(int sock, void *buf, int len);
+
+int read_all(int sock, void *buf, int len);
+
+#endif
