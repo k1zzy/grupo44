d6f87843caaad4f26a45eb165ff686f56a49f979
Guilherme Ramos <fc61840@alunos.fc.ul.pt>
2025-10-24 18:50:35 +0100

    fixes dnv(acho que msg opcode e c_type no list_skell tao erradas em alguns casos)

--- source/list_skel.c
+++ source/list_skel.c
@@ -22,15 +22,22 @@ int list_skel_destroy(struct list_t *list) {
 }
 
 int invoke(MessageT *msg, struct list_t *list) {
-    if (!msg || !list) return -1;
+    if (!msg || !list) {
+        if (msg) {
+            msg->opcode = MESSAGE_T__OPCODE__OP_ERROR;
+            msg->c_type = MESSAGE_T__C_TYPE__CT_NONE;
+        }
+        return -1;
+    }
 
     switch (msg->opcode) {
     case MESSAGE_T__OPCODE__OP_ADD: {
-        /* request: msg->data (Data). response: CT_RESULT with result from list_add */
-        if (msg->c_type != MESSAGE_T__C_TYPE__CT_DATA || !msg->data)
+        if (msg->c_type != MESSAGE_T__C_TYPE__CT_DATA || !msg->data) {
+            msg->opcode = MESSAGE_T__OPCODE__OP_ERROR;
+            msg->c_type = MESSAGE_T__C_TYPE__CT_NONE;
             return -1;
+        }
         Data *pd = msg->data;
-        /* converter Data -> struct data_t */
         struct data_t car;
         memset(&car, 0, sizeof(car));
         car.ano = pd->ano;
@@ -40,25 +47,37 @@ int invoke(MessageT *msg, struct list_t *list) {
         car.combustivel = (enum combustivel_t)pd->combustivel;
 
         int r = list_add(list, &car);
+        if (r < 0) {
+            msg->opcode = MESSAGE_T__OPCODE__OP_ERROR;
+            msg->c_type = MESSAGE_T__C_TYPE__CT_NONE;
+            return -1;
+        }
+        msg->opcode = MESSAGE_T__OPCODE__OP_ADD + 1
         msg->c_type = MESSAGE_T__C_TYPE__CT_RESULT;
         msg->result = r;
-        /* leave msg->data as-is (it will be freed by message_t__free_unpacked) */
         break;
     }
 
     case MESSAGE_T__OPCODE__OP_DEL: {
-        /* request: msg->models[0] contains model string. response: CT_RESULT */
-        if (msg->c_type != MESSAGE_T__C_TYPE__CT_MODEL || msg->n_models < 1 || !msg->models)
+        if (msg->c_type != MESSAGE_T__C_TYPE__CT_MODEL || msg->n_models < 1 || !msg->models) {
+            msg->opcode = MESSAGE_T__OPCODE__OP_ERROR;
+            msg->c_type = MESSAGE_T__C_TYPE__CT_NONE;
             return -1;
+        }
         const char *model = msg->models[0];
         int r = list_remove_by_model(list, model);
+        if (r < 0) {
+            msg->opcode = MESSAGE_T__OPCODE__OP_ERROR;
+            msg->c_type = MESSAGE_T__C_TYPE__CT_NONE;
+            return -1;
+        }
+        msg->opcode = MESSAGE_T__OPCODE__OP_DEL + 1
         msg->c_type = MESSAGE_T__C_TYPE__CT_RESULT;
         msg->result = r;
         break;
     }
 
     case MESSAGE_T__OPCODE__OP_GET: {
-        /* request: marca in msg->result. response: CT_DATA with single Data or CT_RESULT if none */
         enum marca_t marca = (enum marca_t)msg->result;
         struct data_t *found = list_get_by_marca(list, marca);
         if (!found) {
@@ -66,13 +85,11 @@ int invoke(MessageT *msg, struct list_t *list) {
             msg->result = 1; /* not found */
             break;
         }
-        /* build protobuf Data from struct data_t and attach to msg */
         Data *pd = malloc(sizeof(Data));
         if (!pd) {
-            /* free returned struct? leave to caller/list implementation */
-            msg->c_type = MESSAGE_T__C_TYPE__CT_RESULT;
-            msg->result = -1;
-            break;
+            msg->opcode = MESSAGE_T__OPCODE__OP_ERROR;
+            msg->c_type = MESSAGE_T__C_TYPE__CT_NONE;
+            return -1;
         }
         data__init(pd);
         pd->ano = found->ano;
@@ -81,14 +98,13 @@ int invoke(MessageT *msg, struct list_t *list) {
         pd->modelo = found->modelo ? strdup(found->modelo) : NULL;
         pd->combustivel = (Combustivel)found->combustivel;
 
+        msg->opcode = MESSAGE_T__OPCODE__OP_GET + 1
         msg->c_type = MESSAGE_T__C_TYPE__CT_DATA;
         msg->data = pd;
-        /* do not free 'found' here (depends on list implementation ownership) */
         break;
     }
 
     case MESSAGE_T__OPCODE__OP_GETLISTBYYEAR: {
-        /* request: year in msg->result. response: CT_LIST with cars array */
         int year = msg->result;
         struct data_t **arr = list_get_by_year(list, year);
         if (!arr) {
@@ -96,32 +112,24 @@ int invoke(MessageT *msg, struct list_t *list) {
             msg->result = 1; /* not found / empty */
             break;
         }
-
-        /* count items */
         size_t n = 0;
         while (arr[n] != NULL) n++;
-
         if (n == 0) {
             msg->c_type = MESSAGE_T__C_TYPE__CT_RESULT;
             msg->result = 1;
-            /* free arr if list_get_by_year allocated it - leave responsibility to list implementation */
             break;
         }
-
-        /* allocate protobuf Data* array */
         msg->cars = malloc(sizeof(Data *) * n);
         if (!msg->cars) {
-            msg->c_type = MESSAGE_T__C_TYPE__CT_RESULT;
-            msg->result = -1;
-            break;
+            msg->opcode = MESSAGE_T__OPCODE__OP_ERROR;
+            msg->c_type = MESSAGE_T__C_TYPE__CT_NONE;
+            return -1;
         }
         msg->n_cars = n;
         for (size_t i = 0; i < n; ++i) {
             Data *pd = malloc(sizeof(Data));
-            if (!pd) { /* on allocation failure set what we have and return error */
-                msg->c_type = MESSAGE_T__C_TYPE__CT_RESULT;
-                msg->result = -1;
-                /* free previously allocated */
+            if (!pd) {
+                /* rollback allocated entries */
                 for (size_t j = 0; j < i; ++j) {
                     if (msg->cars[j]->modelo) free(msg->cars[j]->modelo);
                     free(msg->cars[j]);
@@ -129,7 +137,9 @@ int invoke(MessageT *msg, struct list_t *list) {
                 free(msg->cars);
                 msg->cars = NULL;
                 msg->n_cars = 0;
-                break;
+                msg->opcode = MESSAGE_T__OPCODE__OP_ERROR;
+                msg->c_type = MESSAGE_T__C_TYPE__CT_NONE;
+                return -1;
             }
             data__init(pd);
             pd->ano = arr[i]->ano;
@@ -139,15 +149,19 @@ int invoke(MessageT *msg, struct list_t *list) {
             pd->combustivel = (Combustivel)arr[i]->combustivel;
             msg->cars[i] = pd;
         }
-        if (msg->c_type != MESSAGE_T__C_TYPE__CT_RESULT) {
-            msg->c_type = MESSAGE_T__C_TYPE__CT_LIST;
-        }
-        /* do not free arr or its elements here - leave to list implementation or caller policy */
+        msg->opcode = MESSAGE_T__OPCODE__OP_GETLISTBYYEAR + 1;
+        msg->c_type = MESSAGE_T__C_TYPE__CT_LIST;
         break;
     }
 
     case MESSAGE_T__OPCODE__OP_ORDER: {
         int r = list_order_by_year(list);
+        if (r < 0) {
+            msg->opcode = MESSAGE_T__OPCODE__OP_ERROR;
+            msg->c_type = MESSAGE_T__C_TYPE__CT_NONE;
+            return -1;
+        }
+        msg->opcode = MESSAGE_T__OPCODE__OP_ORDER + 1
         msg->c_type = MESSAGE_T__C_TYPE__CT_RESULT;
         msg->result = r;
         break;
@@ -155,6 +169,12 @@ int invoke(MessageT *msg, struct list_t *list) {
 
     case MESSAGE_T__OPCODE__OP_SIZE: {
         int r = list_size(list);
+        if (r < 0) {
+            msg->opcode = MESSAGE_T__OPCODE__OP_ERROR;
+            msg->c_type = MESSAGE_T__C_TYPE__CT_NONE;
+            return -1;
+        }
+        msg->opcode = MESSAGE_T__OPCODE__OP_SIZE + 1
         msg->c_type = MESSAGE_T__C_TYPE__CT_RESULT;
         msg->result = r;
         break;
@@ -167,26 +187,35 @@ int invoke(MessageT *msg, struct list_t *list) {
             msg->result = 1;
             break;
         }
-        /* count models */
         size_t n = 0;
         while (models[n] != NULL) n++;
-
         msg->models = malloc(sizeof(char *) * n);
         if (!msg->models) {
-            msg->c_type = MESSAGE_T__C_TYPE__CT_RESULT;
-            msg->result = -1;
-            break;
+            msg->opcode = MESSAGE_T__OPCODE__OP_ERROR;
+            msg->c_type = MESSAGE_T__C_TYPE__CT_NONE;
+            return -1;
         }
         msg->n_models = n;
         for (size_t i = 0; i < n; ++i) {
             msg->models[i] = strdup(models[i] ? models[i] : "");
+            if (!msg->models[i]) {
+                for (size_t j = 0; j < i; ++j) free(msg->models[j]);
+                free(msg->models);
+                msg->models = NULL;
+                msg->n_models = 0;
+                msg->opcode = MESSAGE_T__OPCODE__OP_ERROR;
+                msg->c_type = MESSAGE_T__C_TYPE__CT_NONE;
+                return -1;
+            }
         }
+        msg->opcode = MESSAGE_T__OPCODE__OP_GETMODELS + 1
         msg->c_type = MESSAGE_T__C_TYPE__CT_LIST;
-        /* do not free 'models' here - follow list API ownership semantics */
         break;
     }
 
     default:
+        msg->opcode = MESSAGE_T__OPCODE__OP_ERROR;
+        msg->c_type = MESSAGE_T__C_TYPE__CT_NONE;
         return -1;
     }
 
